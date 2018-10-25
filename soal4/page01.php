10e58b8f0b2e43e31746150b15844d5990f6cb80
Muhammad Sufi "CRYTLZE" Aulia <41355094+crytlze@users.noreply.github.com>
2018-10-25 21:54:35 +0700

    Update soal4/page01.php

--- soal4/page01.php
+++ soal4/page01.php
@@ -1,1 +1,11 @@
-<?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SLIP GAJI KARYAWAN</title>
+</head>
+<body>
+	<form method="POST" action="page02.php">
+		<legend>SLIP GAJI KARYAWAN</legend>
+	</form>
+</body>
+</html>
